d012fd48c196ff733bc64ef54c01ef3f454ffa75
Ben McAvoy <ben.mcavoy@tutanota.com>
2025-04-18 23:49:00 +0100

    feat: malloc & realloc tracking

--- SMTracy/src/dllmain.cpp
+++ SMTracy/src/dllmain.cpp
@@ -1,26 +1,53 @@
-#include <Windows.h>
+#define TRACY_ENABLE
+#define WIN32_LEAN_AND_MEAN
+
+#include <windows.h>
 #include <iostream>
+#include "minhook.h"
+#include "tracy/Tracy.hpp"
 
-#include "log.h"
+typedef void* (__cdecl* malloc_t)(size_t size);
+typedef void* (__cdecl* realloc_t)(void* ptr, size_t new_size);
 
-// DllMain function which gets called when the DLL is loaded
-BOOL APIENTRY DllMain(HMODULE hModule, DWORD dwReason, LPVOID lpReserved) {
-	// RAII object to handle console creation/destruction
-	// Simply having this line here will allow you to print to the games console
-	// Don't worry about it, but if you're interested feel free to look in `utils.h`
-	// for the implementation
-	Log_t logRAII;
+// Original function pointers
+malloc_t original_malloc = nullptr;
+realloc_t original_realloc = nullptr;
 
-	if (dwReason == DLL_PROCESS_ATTACH) {
-		DisableThreadLibraryCalls(hModule);
-		std::cout << "DLL_PROCESS_ATTACH" << std::endl;
-		// Setup, hook, etc here
-	}
+void* __cdecl hooked_malloc(size_t size) {
+	ZoneScopedN("malloc");
+    return original_malloc(size);
+}
 
-	if (dwReason == DLL_PROCESS_DETACH) {
-		std::cout << "DLL_PROCESS_DETACH" << std::endl;
-		// Cleanup, unhooking, etc here
-	}
+void* __cdecl hooked_realloc(void* ptr, size_t new_size) {
+    ZoneScopedN("realloc");
+    return original_realloc(ptr, new_size);
+}
 
-	return TRUE;
+void* get_crt_symbol(const char* name) {
+    HMODULE crt = GetModuleHandleA("ucrtbase.dll");
+    if (!crt) crt = GetModuleHandleA("msvcrt.dll");
+    return crt ? GetProcAddress(crt, name) : nullptr;
+}
+
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved) {
+    if (ul_reason_for_call == DLL_PROCESS_ATTACH) {
+        MH_Initialize();
+
+        void* malloc_addr = get_crt_symbol("malloc");
+        void* realloc_addr = get_crt_symbol("realloc");
+
+        if (malloc_addr) {
+            MH_CreateHook(malloc_addr, &hooked_malloc, reinterpret_cast<void**>(&original_malloc));
+            MH_EnableHook(malloc_addr);
+        }
+
+        if (realloc_addr) {
+            MH_CreateHook(realloc_addr, &hooked_realloc, reinterpret_cast<void**>(&original_realloc));
+            MH_EnableHook(realloc_addr);
+        }
+    }
+    else if (ul_reason_for_call == DLL_PROCESS_DETACH) {
+        MH_Uninitialize();
+    }
+    return TRUE;
 }
